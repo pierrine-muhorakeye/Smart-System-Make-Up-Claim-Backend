374efe12525e1552400cfa51f28105bf94983513
pierrine-muhorakeye <pierrinemuhorakeye@gmail.com>
2023-11-01 02:16:43 -0700

    update index.js server
    running

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+      console.log(`Server is runningS on port ${port}...`)
     );
   } catch (error) {
     console.log(error);
